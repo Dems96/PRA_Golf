3686b20fe223507e5e2b21e38eff164abfbbfcf6
Dems96 <dembabalde.db@gmail.com>
2019-11-06 22:01:04 +0100

    [bop]

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trou;
+use App\Form\TrouType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+
+}
